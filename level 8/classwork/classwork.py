6aec14f896e146c55debc9ebfca639180009af9d
matechubinidze <chubinidzemate053@gmail.com>
2024-11-02 10:08:59 -0700

    Love Goa <3

---
+++ level 8/classwork/classwork.py
@@ -0,0 +1,11 @@
+age = input("Enter Your Age: ")
+surname = input("Enter Your Surname: ")
+name = input("Enter Your Name: ")
+num1 = input("number1: ")
+num2 = input("number2: ")
+
+print(type(age))
+print(type(surname))
+print(type(name))
+print(type(num1))
+print(type(num2))
