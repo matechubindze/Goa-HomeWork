edb5f6cd85a7324f1e46bb0264f440d755fc72f1
matechubinidze <chubinidzemate053@gmail.com>
2024-11-05 08:13:35 -0800

    Love Goa <3

---
+++ level 8/homework/homework.py
@@ -0,0 +1,20 @@
+#რა არის input და output
+#input არის როდესაც რაგაცა ბრძანება მაგალითად კლავიატურაზე ხელის დაჭერა და ამით ტექსტის დაწერა არის input რადგან კომპიუტერს მიეცემა ბრძანება რომელსაც აკეთებს შიგნით მაგალითად ტექსტის შენახვა და ასე შემდეგ
+#Output არის როდესაც ჩვენ კომპიუტერს მივცემთ ბრძანებას და იგი ამას გამოიტანს მაგალიტად ჩვენ პითონში გამოვიყენეთ ბრძანება print და მან print-ში ჩაწერილი მასალა დაგვიბეჭდა
+name = input("Enter your name here: ")
+surname = input("Enter your surname here: ")
+
+print(type(name))
+print(type(surname))
+
+num1 = 10
+num2 = 12
+num3 = 45
+num4 = 67
+num5 = 77
+
+print(num1 + num3)
+print(num4 * num5)
+print(num2 ** num1)
+print(num5 % num4)
+print(num3 / num4)
