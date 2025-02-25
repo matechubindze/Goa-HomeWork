16494b8d186836e33d9f15f5c136c134d87989a9
matechubindze <chubinidzemate053@gmail.com>
2025-02-25 10:16:42 -0800

    goa

---
+++ level 42/classwork/cw.py
@@ -0,0 +1,13 @@
+
+#dict
+dict1 = {
+    "name": "mate",
+    "surname": "chubinidze",
+    "academy": "GOA",
+    "fav-color": "white,black",
+    "city": "Tbilisi",
+    "goa-students": "Mate Chubinidze",
+    "age": "15",
+    "fav-lang": "HTML,CSS,JAVA"
+}
+print(dict1["name";"surname"])
