04a6ba99193247c92a8db0964ec54861671cdae0
matechubindze <chubinidzemate053@gmail.com>
2025-03-01 10:21:28 -0800

    goa cw/hw

--- level 40/homework/codewars.py
+++ level 40/homework/codewars.py
@@ -2,5 +2,13 @@
 def paperwork(n, m):
     return n * m if n > 0 and m > 0 else 0
 #Number 2
+def past(h, m, s):
+    return (h * 3600000) + (m * 60000) + (s * 1000)
+#Number 3
 def are_you_playing_banjo(name):
     return name + " plays banjo" if name[0].lower() == 'r' else name + " does not play banjo"
+#Number 4
+def make_upper_case(s):
+    return s.upper()
+#Number 5
+
