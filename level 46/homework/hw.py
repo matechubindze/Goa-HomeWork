1f588df73f0b8447ac6b495414c52617eb6d9bf8
matechubindze <chubinidzemate053@gmail.com>
2025-03-22 05:52:23 -0700

    goa hw

---
+++ level 46/homework/hw.py
@@ -0,0 +1,18 @@
+#1
+numbers = [x for x in range(1,11)]
+print(numbers)
+#2
+numbers2 = [x**2 for x in range(1,6)]
+print(numbers2)
+#3
+words = ["banana","goa","group56"]
+letters = [words[0] for words in words]
+print(letters)
+#4
+words2 = ["goa","best","the","academy"]
+uppercase = [words2.upper() for words2 in words2]
+print(uppercase)
+#5
+numbers3 = [x for x in range(1,51,3)]
+print(numbers3)
+#6
