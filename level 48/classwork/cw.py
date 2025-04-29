0162eafc8d659064f8b6da7e4c1894c0c9d450d3
matechubindze <chubinidzemate053@gmail.com>
2025-04-29 07:57:12 -0700

    goa:

---
+++ level 48/classwork/cw.py
@@ -0,0 +1,15 @@
+#1
+def remove_every_second(arr):
+    return arr[::2]
+#2
+def square(num):
+    return num ** 2
+
+result = square(7)
+print(result)
+#3
+def monkey_count(n):
+    return list(range(1, n + 1))
+
+result = monkey_count(5)
+print(result)
