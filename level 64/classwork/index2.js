1cd782d93052a1afb9568b7c78bb39a45a8b63b9
matechubindze <chubinidzemate053@gmail.com>
2025-05-20 09:11:16 -0700

    goa:

---
+++ level 64/classwork/index2.js
@@ -0,0 +1,7 @@
+function concStrings() {
+    let firstString = prompt("Enter the first string:");
+    let secondString = prompt("Enter the second string:");
+    alert(firstString + secondString);
+}
+
+concStrings();
